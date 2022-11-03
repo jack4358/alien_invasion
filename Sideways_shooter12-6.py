d01a7b1d42c7f7f26005bd114ed891070fb29122
jack4358 <115736680+jack4358@users.noreply.github.com>
2022-11-03 15:33:34 -0400

    patches

--- Sideways_shooter12-6.py
+++ Sideways_shooter12-6.py
@@ -59,6 +59,8 @@ class AlienInvasion:
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 
@@ -115,6 +117,42 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from teh ship."""
+
+    def __init__(self, ai_game):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color - self.settings.bullet_color
+
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Store the bullet's position as a decimal value.
+        self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.x -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.x = self.x
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
 
 
 if __name__ == '__main__':
